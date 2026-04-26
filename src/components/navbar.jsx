0dd82ed14d9bb0e6c78b89c46ce6bdcf45f32409
Safikkhan31 <safikbihari2@gmail.com>
2026-04-26 11:58:11 +0530

    added css to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import './navbar.css';
 
 export function Navbar() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
+        <nav className="navbar">
+            <h2 className="logo">Portfolio</h2>
+            <ul className="nav-links">
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/projects">Projects</Link></li>
+            </ul>
         </nav>
     )
 }
